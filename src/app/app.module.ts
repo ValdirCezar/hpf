ec514d8fcca34d02d7c41c34272fc21bd9c4e751
Valdir Cezar <valdircezar312@gmail.com>
2021-06-07 11:46:05 -0300

    HTML e CSS para componente HEADER

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/template/header/header.component';
 
+// Imports para componentes do Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +17,9 @@ import { HeaderComponent } from './components/template/header/header.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    // Angular Material
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
