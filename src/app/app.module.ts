e303dc9c3c67275e442d0ecf89b254aba3226e66
Valdir Cezar <valdircezar312@gmail.com>
2021-06-09 15:24:22 -0300

    Componente CLIENTE-DELETE criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -40,6 +40,7 @@ import { TecnicoDeleteComponent } from './components/tecnico/tecnico-delete/tecn
 import { ClienteListComponent } from './components/cliente/cliente-list/cliente-list.component';
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
+import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
 
 @NgModule({
   declarations: [
@@ -56,6 +57,7 @@ import { ClienteUpdateComponent } from './components/cliente/cliente-update/clie
     ClienteListComponent,
     ClienteCreateComponent,
     ClienteUpdateComponent,
+    ClienteDeleteComponent,
   ],
   imports: [
     BrowserModule,
